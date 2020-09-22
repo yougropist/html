678b53ab2321906ac7a903fb7dbd263622c6d993
gropist <afkir.younes@hotmail.com>
2020-09-22 15:58:52 +0200

    ok

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 80;
+const port = 8412;
 const cors = require('cors')
 const connexion = require('./conf.js');
 require('dotenv').config();
 const sendgrid = require('@sendgrid/mail');
 
 app.use(cors({
-    origin: 'http://51.68.175.96:80'
+    origin: 'http://51.68.175.96:8412'
 }))
 
 app.use(bodyParser.json({limit: '10mb'}))
