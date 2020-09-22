9196ca7ffebfdb1628f51df7270bf7969d1e45d4
gropist <afkir.younes@hotmail.com>
2020-09-22 13:47:46 +0200

    ok

--- backend/conf.js
+++ backend/conf.js
@@ -2,11 +2,11 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 const connexion = mysql.createConnection({
-  host : process.env.HOST,
-  user : process.env.USER,
-  password : process.env.PASSWORD,
-  database : process.env.DATABASE,
-  insecureAuth : false
+  host : env.HOST,
+  user : env.USER,
+  password : env.PASSWORD,
+  database : env.DATABASE,
+  insecureAuth : true
 })
 
 connexion.connect(function(err) {
